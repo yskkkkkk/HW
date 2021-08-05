044afffa740d32cd5dd614ca9dcea4f3c1e85165
fbeotjd <fb1014@naver.com>
2021-08-05 14:30:55 +0900

    feat: stack practice,

---
+++ src/com/yskkkkkk/august/algorithm/PairUseStack.java
@@ -0,0 +1,53 @@
+package com.yskkkkkk.august.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class PairUseStack {
+
+	public static void main(String[] args) throws IOException{
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] answer = new int[1];
+		
+		for (int i = 0; i < answer.length; i++) {
+			int size = Integer.parseInt(br.readLine());
+			Stack<Character> stack = new Stack<>();
+			String input = br.readLine();
+			for (int j = 0; j < input.length(); j++) {
+				stack.push(input.charAt(j)); 
+			}
+			int[] sums = new int[4];
+			outer: for (int j = 0; j < size; j++) {
+				switch (stack.pop()) {
+					case '[':	sums[0]+=1;	break;
+					case ']':	sums[0]-=1; break;
+					case '{':	sums[1]+=2;	break;
+					case '}':	sums[1]-=2;	break;
+					case '(':	sums[2]+=3;	break;
+					case ')':	sums[2]-=3;	break;
+					case '<':	sums[3]+=4;	break;
+					case '>':	sums[3]-=4;	break;
+				}
+				for (int k = 0; k < sums.length; k++) {
+					if (sums[k] > 0) {
+						sums[k] = -1;
+						break outer;
+					}
+				}
+			}
+			int sum = 0;
+			for (int j : sums) {
+				sum += j;
+			}
+			answer[i] = sum == 0 ? 1 : 0;
+		}
+		for (int i = 0; i < answer.length; i++) {
+			System.out.println("#"+(i+1)+" "+answer[i]);
+		}
+		br.close();
+	}
+}
