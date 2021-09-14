681c1d8edd59d29cdb9959d3505bd0956ee03100
fbeotjd <fb1014@naver.com>
2021-09-14 17:48:16 +0900

    feat: baekjoon 1로 만들기 dp 기본

---
+++ src/com/yskkkkkk/september/algorithm/MakeOne.java
@@ -0,0 +1,27 @@
+package com.yskkkkkk.september.algorithm;
+
+import java.util.Scanner;
+
+public class MakeOne {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+
+		int arr[] = new int[n + 1];
+
+		arr[1] = 0;
+		for (int i = 2; i <= n; i++) {
+			arr[i] = arr[i - 1] + 1;
+			if (i % 2 == 0)
+				arr[i] = Math.min(arr[i], arr[i / 2] + 1);
+			if (i % 3 == 0)
+				arr[i] = Math.min(arr[i], arr[i / 3] + 1);
+		}
+		
+		System.out.println(arr[n]);
+
+	}
+}
